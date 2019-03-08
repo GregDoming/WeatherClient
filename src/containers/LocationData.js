e177a12b54345203a99633723a14fcda7e77630b
GregDoming <greg.domingue1@gmail.com>
2019-03-07 22:05:41 -0800

    setting state with weather values through a helper function called parseforcast.

--- src/containers/LocationData.js
+++ src/containers/LocationData.js
@@ -6,7 +6,46 @@ import Input from '../components/LocationInput';
 
 class LocationData extends Component {
   state = {
-    location: '',
+    location: 'hi',
+    forecast: 
+    [      
+      {
+        day: 'sunday',
+        temp: 0,
+        weather: ''    
+      },
+      {
+        day: 'monday',
+        temp: 0,
+        weather: ''    
+      }, 
+      {
+        day: 'tuesday',
+        temp: 0,
+        weather: ''    
+      }, 
+      {
+        day: 'wednesday',
+        temp: 0,
+        weather: ''
+      
+      }, 
+      {
+        day: 'thursday',
+        temp: 0,
+        weather: ''     
+      },
+      {
+        day: 'friday',
+        temp: 0,
+        weather: ''     
+      }, 
+      {
+        day: 'saturday',
+        temp: 0,    
+        weather: ''
+      },
+    ]
   }
 
   handleLocationSubmit = async (event) => {
@@ -17,12 +56,39 @@ class LocationData extends Component {
     try {
       const url = `/api/forecast?${location}`;
       const response = await fetch(url);
-      const locationData = await response.text();
-      console.log(locationData);
+      const locationData = await response.json();
+      const forecastObj = locationData.consolidated_weather
+      // console.log(forecastObj)
+
+      // console.log(forecastObj[0].the_temp)
+
+      const forecast = this.parseForecast(forecastObj);
+      this.setState(forecast);
+      console.log(this.state.forecast)
+
+      
     } catch (error) {
       console.log('Error!', error);
     }
   }
+
+  parseForecast = (obj) => {
+    // const weekArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const date = new Date();
+    const tempForecast = Object.assign(this.state.forecast);
+
+    let day = date.getDay();
+    let count = 0;
+
+    while (count < obj.length) {
+      tempForecast[day].temp = obj[count].the_temp;
+      tempForecast[day].weather = obj[count].weather_state_name;
+
+      count += 1;
+      day += 1;
+      if (day > 6) day = 0;
+    }
+  };
   
   handleLocationChange = (event) => {
     this.setState({ location: event.target.value });
---
+++ src/containers/WeatherForcast.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class WeatherForecast extends Component {
+  state = {
+    forcast: []
+  }
+  
+
+
+}
